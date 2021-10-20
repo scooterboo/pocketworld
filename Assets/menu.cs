09bca4b373dab9d447d86fa0fd01bb7c8acb15da
scooterboo <lewasite@yahoo.com>
2021-10-19 18:08:02 -0700

    add menu button and update to 2020.3.20f1 (LTS)

--- Assets/menu.cs
+++ Assets/menu.cs
@@ -15,12 +15,10 @@ public class menu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Texture2D menu = new Texture2D(2, 2);
-        menu.LoadImage(File.ReadAllBytes(WorldTilemap.GetComponent<the_world>().texturefolder + "\\BlueNA\\menusheet.png"));
-        //Texture2D menu = Resources.Load<Texture2D>("menusheet");
-        menu.filterMode = FilterMode.Point;
-        Sprite menusprite = Sprite.Create(menu, new Rect(0, 0, menu.width, menu.height), new Vector2(0.5f, 0.5f), 16.0f, 1, SpriteMeshType.FullRect, new Vector4(8, 8, 8, 8));
-        //gameObject.transform.localPosition = new Vector3(0f, -5.5f, 0f);
+        Texture2D sidemenu = new Texture2D(2, 2);
+        sidemenu.LoadImage(File.ReadAllBytes(WorldTilemap.GetComponent<the_world>().texturefolder + "\\BlueNA\\menusheet.png"));
+        sidemenu.filterMode = FilterMode.Point;
+        Sprite menusprite = Sprite.Create(sidemenu, new Rect(0, 0, sidemenu.width, sidemenu.height), new Vector2(0.5f, 0.5f), 16.0f, 1, SpriteMeshType.FullRect, new Vector4(8, 8, 8, 8));
         SpriteRenderer sr = gameObject.AddComponent<SpriteRenderer>();
         sr.sprite = menusprite;
         sr.drawMode = SpriteDrawMode.Sliced;
@@ -28,11 +26,18 @@ public class menu : MonoBehaviour
 
         menuselector.localPosition = new Vector3(0f, -4.5f, -0.5f);
 
-
         for (int i = 0; i <= 8; ++i)
         {
-            MenuTilemap.SetTile(new Vector3Int(0, i-1, 0), WorldTilemap.GetComponent<the_world>().tiles[i]);
+            MenuTilemap.SetTile(new Vector3Int(0, i - 1, 0), WorldTilemap.GetComponent<the_world>().tiles[i]);
         }
+
+        Texture2D menubutton = Resources.Load<Texture2D>("menu");
+        Tile menutile = ScriptableObject.CreateInstance<Tile>();
+        menutile.sprite = Sprite.Create(menubutton, new Rect(0f, 0f, 16f, 16f), new Vector2(0.5f, 0.5f), 16.0f);
+        menutile.sprite.texture.filterMode = FilterMode.Point;
+        menutile.name = "0";
+        MenuTilemap.SetTile(new Vector3Int(0, -1, 0), menutile);
+
     }
 
     // Update is called once per frame
@@ -67,11 +72,18 @@ public class menu : MonoBehaviour
             {
                 Vector3 pos = menuselector.localPosition;
                 pos.y = Mathf.Floor(.5f+(mpos.y - ppos.y) / 1.5f) * 1.5f;
-                if (pos.y > 6) pos.y = 6;
-                if (pos.y < -6) pos.y = -6;
-                menuselector.localPosition = pos;
-                SelectorTilemap.GetComponent<SelectorGrid>().currtile = int.Parse(MenuTilemap.GetTile<Tile>(new Vector3Int((int)menuselector.localPosition.x, (int)(3.5f + menuselector.localPosition.y / 1.5f), 0)).name);
-                SelectorTilemap.GetComponent<SelectorGrid>().UpdateSprite();
+                if (pos.y > 6) pos.y = 6f;
+                if (pos.y < -6) pos.y = -6f;
+                if (pos.y == -6f)
+                {
+                    print("menu!");
+                }
+                else
+                {
+                    menuselector.localPosition = pos;
+                    SelectorTilemap.GetComponent<SelectorGrid>().currtile = int.Parse(MenuTilemap.GetTile<Tile>(new Vector3Int((int)menuselector.localPosition.x, (int)(3.5f + menuselector.localPosition.y / 1.5f), 0)).name);
+                    SelectorTilemap.GetComponent<SelectorGrid>().UpdateSprite();
+                }
             }
 
         }
